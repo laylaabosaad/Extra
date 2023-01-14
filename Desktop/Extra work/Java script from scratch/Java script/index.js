d04d3c852888c3a9d4a6ba6711b59070c2851bb5
layla <laylaabosaad@gmail.com>
2023-01-14 14:08:04 +0200

    train station

---
+++ Desktop/Extra work/Java script from scratch/Java script/index.js
@@ -0,0 +1,21 @@
+let count=0
+let countEl=document.getElementById("count-el")
+let saveEl=document.getElementById("save")
+
+
+
+function add(){
+count+=1;
+countEl.innerText= count;
+}
+
+
+
+function save(){
+    let save= count +" - ";
+    saveEl.textContent+= save;
+    countEl.innerText= 0;
+    count=0;
+
+
+}
